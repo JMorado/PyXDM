5cb3323247e9491406f5e96ce976f11a3f64ac6b
Joao Morado <joaomorado@gmail.com>
2025-10-09 21:47:55 +0100

    Fix closed/open shell implementation

--- pyxdm/core/core.py
+++ pyxdm/core/core.py
@@ -38,64 +38,45 @@ class XDMCalculator:
     def _compute_density_properties(self, grid_points: np.ndarray, anisotropic: bool = True) -> dict:
         """Compute density and derived properties on grid points."""
         if self._closed_shell:
-            rho = self.mol.obasis.compute_grid_density_dm(self.dm_full, grid_points)
-            tau = 2.0 * self.mol.obasis.compute_grid_kinetic_dm(self.dm_full, grid_points)
-            nabla = self.mol.obasis.compute_grid_gradient_dm(self.dm_full, grid_points)
-            hessian = self.mol.obasis.compute_grid_hessian_dm(self.dm_full, grid_points)
-
-            laplacian = hessian[:, 0] + hessian[:, 3] + hessian[:, 5]
-            nabla_mag2 = np.sum(nabla**2, axis=1)
-
-            d = tau - 0.25 * nabla_mag2 / rho
-            Q = (laplacian - 2.0 * d) / 6.0
-
-            b_sigma = compute_b_sigma(rho, Q)
-
-            props = {"rho": rho, "b": b_sigma}
-
-            if anisotropic:
-                # Calculate the normalized gradient vectors and add them to the properties dict
-                epsilon = 1e-12
-                nabla_norm = np.linalg.norm(nabla, axis=1)[:, None]
-                props["u"] = nabla / (nabla_norm + epsilon)
+            rho_alpha = rho_beta = self.mol.obasis.compute_grid_density_dm(self.dm_alpha, grid_points) 
+            tau_alpha = tau_beta = 2.0 * self.mol.obasis.compute_grid_kinetic_dm(self.dm_alpha, grid_points)
+            nabla_alpha = nabla_beta = self.mol.obasis.compute_grid_gradient_dm(self.dm_alpha, grid_points)
+            hessian_alpha = hessian_beta = self.mol.obasis.compute_grid_hessian_dm(self.dm_alpha, grid_points)
+            laplacian_alpha = laplacian_beta = (hessian_alpha[:, 0] + hessian_alpha[:, 3] + hessian_alpha[:, 5])
         else:
             rho_alpha = self.mol.obasis.compute_grid_density_dm(self.dm_alpha, grid_points)
             rho_beta = self.mol.obasis.compute_grid_density_dm(self.dm_beta, grid_points)
-
             tau_alpha = 2.0 * self.mol.obasis.compute_grid_kinetic_dm(self.dm_alpha, grid_points)
             tau_beta = 2.0 * self.mol.obasis.compute_grid_kinetic_dm(self.dm_beta, grid_points)
-
             nabla_alpha = self.mol.obasis.compute_grid_gradient_dm(self.dm_alpha, grid_points)
             nabla_beta = self.mol.obasis.compute_grid_gradient_dm(self.dm_beta, grid_points)
-
             hessian_alpha = self.mol.obasis.compute_grid_hessian_dm(self.dm_alpha, grid_points)
             hessian_beta = self.mol.obasis.compute_grid_hessian_dm(self.dm_beta, grid_points)
-
             laplacian_alpha = hessian_alpha[:, 0] + hessian_alpha[:, 3] + hessian_alpha[:, 5]
             laplacian_beta = hessian_beta[:, 0] + hessian_beta[:, 3] + hessian_beta[:, 5]
 
-            nabla_alpha_mag2 = np.sum(nabla_alpha**2, axis=1)
-            nabla_beta_mag2 = np.sum(nabla_beta**2, axis=1)
+        nabla_alpha_mag2 = np.sum(nabla_alpha**2, axis=1)
+        nabla_beta_mag2 = np.sum(nabla_beta**2, axis=1)
 
-            d_alpha = tau_alpha - 0.25 * nabla_alpha_mag2 / rho_alpha
-            d_beta = tau_beta - 0.25 * nabla_beta_mag2 / rho_beta
+        d_alpha = tau_alpha - 0.25 * nabla_alpha_mag2 / rho_alpha
+        d_beta = tau_beta - 0.25 * nabla_beta_mag2 / rho_beta
 
-            Q_alpha = (laplacian_alpha - 2.0 * d_alpha) / 6.0
-            Q_beta = (laplacian_beta - 2.0 * d_beta) / 6.0
+        Q_alpha = (laplacian_alpha - 2.0 * d_alpha) / 6.0
+        Q_beta = (laplacian_beta - 2.0 * d_beta) / 6.0
+        
+        b_alpha = compute_b_sigma(rho_alpha, Q_alpha)
+        b_beta = compute_b_sigma(rho_beta, Q_beta)
 
-            b_alpha = compute_b_sigma(rho_alpha, Q_alpha)
-            b_beta = compute_b_sigma(rho_beta, Q_beta)
+        props = {"rho_alpha": rho_alpha, "rho_beta": rho_beta, "b_alpha": b_alpha, "b_beta": b_beta}
 
-            props = {"rho_alpha": rho_alpha, "rho_beta": rho_beta, "b_alpha": b_alpha, "b_beta": b_beta}
+        if anisotropic:
+            # Calculate the normalized gradient vectors and add them to the properties dict
+            epsilon = 1e-12
+            nabla_alpha_norm = np.linalg.norm(nabla_alpha, axis=1)[:, None]
+            nabla_beta_norm = np.linalg.norm(nabla_beta, axis=1)[:, None]
 
-            if anisotropic:
-                # Calculate the normalized gradient vectors and add them to the properties dict
-                epsilon = 1e-12
-                nabla_alpha_norm = np.linalg.norm(nabla_alpha, axis=1)[:, None]
-                nabla_beta_norm = np.linalg.norm(nabla_beta, axis=1)[:, None]
-
-                props["u_alpha"] = nabla_alpha / (nabla_alpha_norm + epsilon)
-                props["u_beta"] = nabla_beta / (nabla_beta_norm + epsilon)
+            props["u_alpha"] = nabla_alpha / (nabla_alpha_norm + epsilon)
+            props["u_beta"] = nabla_beta / (nabla_beta_norm + epsilon)
 
         return props
 
@@ -106,71 +87,53 @@ class XDMCalculator:
         r_vec = grid.points - self.mol.coordinates[atom_idx]
         r_i = np.linalg.norm(r_vec, axis=1)
 
-        if self._closed_shell:
-            if not anisotropic:
-                b_capped = np.minimum(density_props["b"], r_i)
-                term_iso = r_i**order - (r_i - b_capped) ** order
-                integrand_iso = density_props["rho"] * term_iso**2
-                lambda_iso = grid.integrate(integrand_iso * weights_i)
-                return lambda_iso, None
-            else:
-                b_capped = np.minimum(density_props["b"], r_i)
-                moment_mag = r_i**order - (r_i - b_capped) ** order
-                term_vec = moment_mag[:, None] * density_props["u"]
-                outer = np.einsum("ni,nj->nij", term_vec, term_vec)
-                integrand = density_props["rho"][:, None, None] * outer
-                lambda_tensor = np.sum(grid.weights[:, None, None] * weights_i[:, None, None] * integrand, axis=0)
-                lambda_iso = np.trace(lambda_tensor)
-                return lambda_iso, lambda_tensor
+        if not anisotropic:
+            logger.debug(f"Calculating isotropic moment for atom {atom_idx} with order {order}")
+            # Capped b_sigma values
+            b_alpha_capped = np.minimum(density_props["b_alpha"], r_i)
+            b_beta_capped = np.minimum(density_props["b_beta"], r_i)
+
+            # The term that gets squared
+            term_alpha_iso = r_i**order - (r_i - b_alpha_capped) ** order
+            term_beta_iso = r_i**order - (r_i - b_beta_capped) ** order
+
+            # The integrand includes the square of the term
+            integrand_alpha = density_props["rho_alpha"] * term_alpha_iso**2
+            integrand_beta = density_props["rho_beta"] * term_beta_iso**2
+
+            # Perform the integration
+            lambda_alpha = grid.integrate(integrand_alpha * weights_i)
+            lambda_beta = grid.integrate(integrand_beta * weights_i)
+
+            lambda_iso = lambda_alpha + lambda_beta
+            return lambda_iso, None
         else:
-            if not anisotropic:
-                logger.debug(f"Calculating isotropic moment for atom {atom_idx} with order {order}")
-                # Capped b_sigma values
-                b_alpha_capped = np.minimum(density_props["b_alpha"], r_i)
-                b_beta_capped = np.minimum(density_props["b_beta"], r_i)
+            logger.debug(f"Calculating anisotropic moment for atom {atom_idx} with order {order}")
+            b_alpha_capped = np.minimum(density_props["b_alpha"], r_i)
+            b_beta_capped = np.minimum(density_props["b_beta"], r_i)
 
-                # The term that gets squared
-                term_alpha_iso = r_i**order - (r_i - b_alpha_capped) ** order
-                term_beta_iso = r_i**order - (r_i - b_beta_capped) ** order
+            # Get the magnitudes of the induced moments
+            moment_mag_alpha = r_i**order - (r_i - b_alpha_capped) ** order
+            moment_mag_beta = r_i**order - (r_i - b_beta_capped) ** order
 
-                # The integrand includes the square of the term
-                integrand_alpha = density_props["rho_alpha"] * term_alpha_iso**2
-                integrand_beta = density_props["rho_beta"] * term_beta_iso**2
+            # CORRECTED: Construct the vector using the density gradient direction
+            term_alpha_vec = moment_mag_alpha[:, None] * density_props["u_alpha"]
+            term_beta_vec = moment_mag_beta[:, None] * density_props["u_beta"]
 
-                # Perform the integration
-                lambda_alpha = grid.integrate(integrand_alpha * weights_i)
-                lambda_beta = grid.integrate(integrand_beta * weights_i)
+            # The rest of your calculation is correct
+            outer_alpha = np.einsum("ni,nj->nij", term_alpha_vec, term_alpha_vec)
+            outer_beta = np.einsum("ni,nj->nij", term_beta_vec, term_beta_vec)
 
-                lambda_iso = lambda_alpha + lambda_beta
-                return lambda_iso, None
+            integrand_alpha = density_props["rho_alpha"][:, None, None] * outer_alpha
+            integrand_beta = density_props["rho_beta"][:, None, None] * outer_beta
 
-            else:
-                logger.debug(f"Calculating anisotropic moment for atom {atom_idx} with order {order}")
-                b_alpha_capped = np.minimum(density_props["b_alpha"], r_i)
-                b_beta_capped = np.minimum(density_props["b_beta"], r_i)
+            lambda_alpha_tensor = np.sum(grid.weights[:, None, None] * weights_i[:, None, None] * integrand_alpha, axis=0)
+            lambda_beta_tensor = np.sum(grid.weights[:, None, None] * weights_i[:, None, None] * integrand_beta, axis=0)
 
-                # Get the magnitudes of the induced moments
-                moment_mag_alpha = r_i**order - (r_i - b_alpha_capped) ** order
-                moment_mag_beta = r_i**order - (r_i - b_beta_capped) ** order
+            lambda_tensor = lambda_alpha_tensor + lambda_beta_tensor
+            lambda_iso = np.trace(lambda_tensor)
 
-                # CORRECTED: Construct the vector using the density gradient direction
-                term_alpha_vec = moment_mag_alpha[:, None] * density_props["u_alpha"]
-                term_beta_vec = moment_mag_beta[:, None] * density_props["u_beta"]
-
-                # The rest of your calculation is correct
-                outer_alpha = np.einsum("ni,nj->nij", term_alpha_vec, term_alpha_vec)
-                outer_beta = np.einsum("ni,nj->nij", term_beta_vec, term_beta_vec)
-
-                integrand_alpha = density_props["rho_alpha"][:, None, None] * outer_alpha
-                integrand_beta = density_props["rho_beta"][:, None, None] * outer_beta
-
-                lambda_alpha_tensor = np.sum(grid.weights[:, None, None] * weights_i[:, None, None] * integrand_alpha, axis=0)
-                lambda_beta_tensor = np.sum(grid.weights[:, None, None] * weights_i[:, None, None] * integrand_beta, axis=0)
-
-                lambda_tensor = lambda_alpha_tensor + lambda_beta_tensor
-                lambda_iso = np.trace(lambda_tensor)
-
-                return lambda_iso, lambda_tensor
+            return lambda_iso, lambda_tensor
 
     def calculate_xdm_moments(self, partition_obj: Any, grid: Any, order: Union[List[int], int], anisotropic: bool = True) -> dict:
         """Calculate XDM multipole moments for all atoms."""
