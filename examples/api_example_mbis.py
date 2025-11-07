9b0ac76672cbf7b0dead992cd5d68decc0711279
Joao Morado <joaomorado@gmail.com>
2025-11-07 16:43:11 +0000

    Update example scripts

---
+++ examples/api_example_mbis.py
@@ -0,0 +1,15 @@
+from pyxdm.core import XDMSession
+from pyxdm.utils.io import write_h5_output
+
+session = XDMSession('water/orca.molden.input')
+session.load_molecule()
+session.setup_grid()
+session.setup_calculator()
+session.setup_partition_schemes(['mbis'])
+xdm_results = session.calculator.calculate_xdm_moments(
+    partition_obj=session.partitions['mbis'],
+    grid=session.grid,
+    order=[1, 2, 3],
+    anisotropic=False,
+)
+write_h5_output('pyxdm.h5', session, xdm_results)
---
+++ examples/api_example_multiple_schemes.py
@@ -0,0 +1,20 @@
+from pyxdm.core import XDMSession
+from pyxdm.utils.io import write_h5_output
+
+SCHEMES = ['mbis', 'iterative-stockholder', 'hirshfeld', 'hirshfeld-i', 'becke']
+
+session = XDMSession('water/orca.molden.input')
+session.load_molecule()
+session.setup_grid()
+session.setup_calculator()
+session.setup_partition_schemes(SCHEMES, proatomdb='proatomdb_wb97x-D3_def2-TZVPP.h5')
+
+for scheme in SCHEMES:
+    print(f"Calculating XDM moments for partitioning scheme: {scheme}")
+    xdm_results = session.calculator.calculate_xdm_moments(
+        partition_obj=session.partitions[scheme],
+        grid=session.grid,
+        order=[1, 2, 3],
+        anisotropic=False,
+    )
+    write_h5_output(f'pyxdm_scheme_{scheme}.h5', session, xdm_results)
